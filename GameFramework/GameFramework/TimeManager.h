2e480deb0f82f85da91fc3de54def777102834ed
SuperCodeMonkey <pkh94949494@gmail.com>
2020-08-12 22:37:59 +0900

    타임매니저 수정본

--- GameFramework/GameFramework/TimeManager.h
+++ GameFramework/GameFramework/TimeManager.h
@@ -16,14 +16,17 @@ public:
 
 private:
 	// Frame Control
-	int frameCounter = 0; // 프레임 카운터(초당 갱신)
-	int targetFPS = 60; // 목표 프레임
-	int fps = 0; // 마지막 FPS (출력용)
+	__int64 frameCounter = 0; // 프레임 카운터(초당 갱신)
+	__int64 targetFPS = 60; // 목표 프레임
+	__int64 fps = 0; // 마지막 FPS (출력용)
 	float deltaTime = 0.f; // 델타타임
-	int targetFrameRate = 0; // 프레임 당 목표 경과 시간
-	DWORD oldTime = 0; // 측정용 이전프레임 시간
-	int timeStack = 0;  // 프레임당 초과/미만 시간(누산)
-	DWORD elapseSum = 0; // 1초 측정용
+	float targetFrameRate = 0; // 프레임 당 목표 경과 시간
+	__int64 oldTime = 0; // 측정용 이전프레임 시간
+	float timeStack = 0;  // 프레임당 초과/미만 시간(누산)
+	__int64 elapseSum = 0; // 1초 측정용
+
+	bool isUsedQueryPerformance = false;
+	LARGE_INTEGER frequency;
 
 };
 
