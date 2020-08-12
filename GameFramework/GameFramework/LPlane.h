1c3f4323313bdf3dc17ca0dfa031aef919bfb9ce
GMH <kvdd6826@naver.com>
2020-08-13 04:32:14 +0900

    적 비행기 만들기

--- GameFramework/GameFramework/LPlane.h
+++ GameFramework/GameFramework/LPlane.h
@@ -1,7 +1,17 @@
 #pragma once
 #include "Character.h"
 class LPlane :
-    public Character
+	public Character
 {
+public:
+	LPlane();
+	virtual ~LPlane();
+
+public:
+	void Render() override;
+	void Update() override;
+
+	float count=0.f;
+	float SecondCount = 0.f;
 };
 
--- GameFramework/GameFramework/MPlane.cpp
+++ GameFramework/GameFramework/MPlane.cpp
@@ -1,2 +1,56 @@
 #include "pch.h"
 #include "MPlane.h"
+
+
+MPlane::MPlane()
+{
+	scale.x = 35;
+	scale.y = 35;
+	speed = 200.f;
+	SecondCount = 0.f;
+}
+
+MPlane::~MPlane()
+{
+}
+
+void MPlane::Update()
+{
+	++count;
+	
+	if (count<=100&&SecondCount==0)
+	{
+		position.x += forceX * speed * TimeManager::DeltaTime();
+		if (count == 100)
+		{
+			
+			speed = -50.f;
+			count = 0;
+			++SecondCount;
+		}
+	}
+	else if (count <= 100 && SecondCount >= 1 &&SecondCount<6)
+	{
+		position.x += forceX * speed * TimeManager::DeltaTime();
+		if (count == 100)
+		{
+			forceX *= -1.f;
+			count = 0;
+			++SecondCount;
+		}
+	}
+	else
+	{
+		speed = 200.f;
+		position.x += forceX * speed * TimeManager::DeltaTime();
+		position.y += speed * TimeManager::DeltaTime();
+	}
+}
+
+
+
+void MPlane::Render()
+{
+	RenderManager::DrawRect(position, rotation, scale);
+
+}
