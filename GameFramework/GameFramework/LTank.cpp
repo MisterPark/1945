30dc08ac61fff86f238d211cefa44010ba28c7f3
SuperCodeMonkey <pkh94949494@gmail.com>
2020-08-13 22:59:51 +0900

    탱크 포신추가

--- GameFramework/GameFramework/LTank.cpp
+++ GameFramework/GameFramework/LTank.cpp
@@ -43,4 +43,13 @@ void LTank::Render()
 {
 	RenderManager::DrawRect(transform.position, transform.rotation, transform.scale);
 
+	GameObject* p = ObjectManager::FindObject(ObjectType::PLAYER);
+	if (p == nullptr) return;
+	D3DXVECTOR3 posinDirection = p->transform.position;
+	posinDirection -= transform.position;
+	D3DXVec3Normalize(&posinDirection, &posinDirection);
+	posinDirection *= 30.f;
+	posinDirection += transform.position;
+	RenderManager::DrawLine(transform.position.x, transform.position.y, posinDirection.x, posinDirection.y);
+
 }
--- GameFramework/GameFramework/MCannon.cpp
+++ GameFramework/GameFramework/MCannon.cpp
@@ -26,4 +26,13 @@ void MCannon::Render()
 {
 	RenderManager::DrawRect(transform.position, transform.rotation, transform.scale);
 
+	GameObject* p = ObjectManager::FindObject(ObjectType::PLAYER);
+	if (p == nullptr) return;
+	D3DXVECTOR3 posinDirection = p->transform.position;
+	posinDirection -= transform.position;
+	D3DXVec3Normalize(&posinDirection, &posinDirection);
+	posinDirection *= 30.f;
+	posinDirection += transform.position;
+	RenderManager::DrawLine(transform.position.x, transform.position.y, posinDirection.x, posinDirection.y);
+
 }
